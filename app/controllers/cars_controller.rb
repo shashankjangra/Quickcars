0e04a66c97d6829d402eb3eb00849741b3b3f7cc
Shashank Jangra <57314546+shashankjangra@users.noreply.github.com>
2020-11-20 11:21:06 +0530

    user model update
    If the user isnt logged in:
    only see all cars
    
    if the user is logged in:
    can see all his listed cars

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,6 +1,7 @@
 class CarsController < ApplicationController
   # skip_before_action :verify_authenticity_token
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user, only: [:edit, :update, :destroy]
 
   def index
       @cars = Car.all
@@ -11,7 +12,8 @@ class CarsController < ApplicationController
   end
 
   def new
-      @car = Car.new
+    #   @car = Car.new
+      @car = current_user.cars.build
   end
 
   def edit
@@ -19,7 +21,8 @@ class CarsController < ApplicationController
   end
 
   def create
-      @car = Car.new(car_params)
+    #   @car = Car.new(car_params)
+    @car = current_user.cars.build(car_params)
       if @car.save
           redirect_to @car     
       else
@@ -42,6 +45,12 @@ class CarsController < ApplicationController
       @car.destroy
       redirect_to cars_path
   end
+
+  def correct_user
+        @car = current_user.cars.find_by(id: params[:id])
+        redirect_to cars_path, notice: "Not authorized to Edit this Car" if @car.nil?
+  end
+
 
   private
   def car_params
