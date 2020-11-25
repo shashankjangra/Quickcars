9901dc441bd3ff0a08ec1399384280dc56ca763f
Shashank Jangra <57314546+shashankjangra@users.noreply.github.com>
2020-11-25 14:38:47 +0530

    slot delete
    working slot delete

--- app/controllers/slots_controller.rb
+++ app/controllers/slots_controller.rb
@@ -1,4 +1,10 @@
 class SlotsController < ApplicationController
+
+    def edit
+        @car = Car.find(params[:car_id])
+        @slot = @car.slots.params(slot_params)
+    end
+
     def create
         @car = Car.find(params[:car_id])
         @slot = @car.slots.create(slot_params)
@@ -6,6 +12,20 @@ class SlotsController < ApplicationController
     end
     
     def slotbook
+        @car = Car.find(params[:car_id])
+        @slot = @car.slot.update(slot_params)
+        if @slot.update(slot_params)
+            redirect_to @car
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @car = Car.find(params[:car_id])
+        @slot = @car.slots.find(params[:slot_id])
+        @slot.destroy
+        redirect_to @car
     end
 
     private
