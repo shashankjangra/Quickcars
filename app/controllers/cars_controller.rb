756e454ae89052cce89fc35c0819386957416eb2
Shashank Jangra <57314546+shashankjangra@users.noreply.github.com>
2020-12-03 15:12:12 +0530

    tiny update

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -3,7 +3,7 @@ class CarsController < ApplicationController
     before_action :correct_user, only: [:edit, :update, :destroy, :dashboard]
 
     def allcars
-        @cars = Car.all
+        @cars = Car.all.limit(10)
     end
 
     def dashboard
